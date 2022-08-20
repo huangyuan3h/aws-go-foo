4ed521fcff616c76a2505aff57720f50bea844ae
huangyuan3h <huangyuan3h@gmail.com>
2022-08-20 15:38:47 +0800

    :recycle: add del fn

--- controller/todosController.go
+++ controller/todosController.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucsky/cuid"
 )
 
 type todo struct {
@@ -12,16 +13,35 @@ type todo struct {
 }
 
 var todos = []todo{
-	{ID: "1", Text: "text1"},
+	{ID: cuid.New(), Text: "text1"},
 }
 
 func getTodos(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, todos)
 }
 
+func postTodos(c *gin.Context) {
+
+	todos = append(todos, todo{cuid.New(), c.PostForm("text")})
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func delTodos(c *gin.Context) {
+	filtered := []todo{}
+	deletedID := c.PostForm("id")
+	for _, item := range todos {
+		if item.ID != deletedID {
+			filtered = append(filtered, item)
+		}
+	}
+	todos = filtered
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
-
+	router.POST("/todos", postTodos)
+	router.DELETE("/todos", delTodos)
 	router.Run("localhost:8080")
 }
