8b90cd295983d6a6d121bfc7378ec208908e1897
huangyuan3h <huangyuan3h@gmail.com>
2022-08-20 11:39:35 +0800

    :recycle: add go gin

--- controller/todosController.go
+++ controller/todosController.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type todo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+}
+
+var todos = []todo{
+	{ID: "1", Text: "text1"},
+}
+
+func getTodos(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, todos)
+}
+
 func main() {
-	fmt.Println("test")
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+
+	router.Run("localhost:8080")
 }
