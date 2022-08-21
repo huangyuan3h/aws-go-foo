df9ffd0ac92fc8d632158e83bbe6f210fa566a1e
huangyuan3h <huangyuan3h@gmail.com>
2022-08-21 14:40:59 +0800

    :recycle: update gorm fn

--- dataAccess/todos.go
+++ dataAccess/todos.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"github.com/lucsky/cuid"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
-	fmt.Println("test")
+type Todo struct {
+	gorm.Model
+	id   string
+	text string
+}
+
+var db *gorm.DB
+
+func getDB() *gorm.DB {
+	dsn := "root:root@tcp(127.0.0.1:3306)/foo?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
+
+// sinple singleton
+func getSingleDB() *gorm.DB {
+	if db == nil {
+		db = getDB()
+	}
+	return db
+}
+
+func addTodo(t string) {
+	db := getSingleDB()
+	db.Create(&Todo{id: cuid.New(), text: t})
+}
+
+func findTodo(id string) Todo {
+	db := getSingleDB()
+	var todo Todo
+	db.First(&todo, 1)               // find Todo with integer primary key
+	db.First(&todo, "id = ?", "D42") // find Todo with code D42
+	return todo
+}
+
+func delTodo(id string) {
+	db := getSingleDB()
+	var todo = findTodo(id)
+	db.Delete(&todo, 1)
 }
