6322e0270a326759a6237ee330f8e424855562c3
huangyuan3h <huangyuan3h@gmail.com>
2022-08-21 16:20:28 +0800

    :recycle: update dot env suport

--- dataAccess/go.mod
+++ dataAccess/go.mod
@@ -3,6 +3,7 @@ module foo.com/dataAccess
 go 1.18
 
 require (
+	github.com/Netflix/go-env v0.0.0-20220526054621-78278af1949d
 	github.com/lucsky/cuid v1.2.1
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
--- dataAccess/todos.go
+++ dataAccess/todos.go
@@ -1,10 +1,24 @@
 package todos
 
 import (
+	"fmt"
+	"log"
+
+	env "github.com/Netflix/go-env"
 	"github.com/lucsky/cuid"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+type Environment struct {
+	DB struct {
+		User     string `env:"DB_User,default=root"`
+		Password string `env:"DB_Password"`
+		Host     string `env:"DB_Host,default=127.0.0.1"`
+		Port     string `env:"DB_Port,default=3306"`
+		Database string `env:"Database,default=foo"`
+	}
+}
 
 type Todo struct {
 	gorm.Model
@@ -14,8 +28,20 @@ type Todo struct {
 
 var db *gorm.DB
 
+func getDSN() string {
+	var environment Environment
+	_, err := env.UnmarshalFromEnviron(&environment)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+		environment.DB.User, environment.DB.Password, environment.DB.Host, environment.DB.Port, environment.DB.Database)
+	return t
+}
+
 func getDB() *gorm.DB {
-	dsn := "root:root@tcp(127.0.0.1:3306)/foo?charset=utf8mb4&parseTime=True&loc=Local"
+
+	dsn := getDSN()
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
